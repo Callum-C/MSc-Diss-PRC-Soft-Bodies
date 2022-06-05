6330aa01ccef364deb84899dc29e1daef8afdf45
Callum-C <60474698+Callum-C@users.noreply.github.com>
2022-06-05 15:54:56 +0100

    Vector Stable

--- 1D Spring.py
+++ 1D Spring.py
@@ -6,10 +6,11 @@ import numpy as np
 FILL =      (45, 197, 244)
 BACKGROUND_COLOR = (112, 50, 126)
 (width, height) = (1280, 720)
-
+mid = (width/2, height/2)
 def main():
-    bob = (300, 300)    # (x, y)
-    anchor = (300, 100) # (x, y)
+    bob = np.array((mid[0], 300))    # (x, y)
+    anchor = np.array((mid)) # (x, y)
+
     rest_length = 150
     k = 0.01
     velocity = 0
@@ -23,11 +24,19 @@ def main():
     running = True
     while running:
         clock.tick(60)
-        spring_length = bob[1] - anchor[1]  # Only subtract y values
+
+        spring_vector = bob - anchor
+        spring_length = np.linalg.norm(spring_vector)
         x = spring_length - rest_length     # Spring Displacement
-        force = -1 * k * x
+
+        v_hat = 0
+        if spring_length != 0:
+            v_hat = spring_vector / spring_length   # Unit vector 
+
+        force = -1 * ((k * x) * v_hat)
+
         velocity += force
-        bob = (bob[0], bob[1] + velocity) 
+        bob[1] += velocity[1]
 
         velocity = velocity * 0.99
 
@@ -38,7 +47,7 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONUP:
                 y = move_bob()
-                bob = (bob[0], y)
+                bob[1] = y
                 velocity = 0
                 update_screen(bob, anchor, screen)
 
@@ -61,7 +70,8 @@ def screen_setup(bob, anchor, screen):
     pygame.display.set_caption("Spring Damper")
     screen.fill(BACKGROUND_COLOR)
 
-    pygame.draw.circle(screen, FILL, bob, 35)
+    pygame.draw.circle(screen, FILL, bob, 32)
+    pygame.draw.circle(screen, FILL, anchor, 16)
     pygame.draw.line(screen, FILL, bob, anchor, 2)
 
     pygame.display.update()
@@ -70,7 +80,8 @@ def screen_setup(bob, anchor, screen):
 def update_screen(bob, anchor, screen):
     screen.fill(BACKGROUND_COLOR)
 
-    pygame.draw.circle(screen, FILL, bob, 35)
+    pygame.draw.circle(screen, FILL, bob, 32)
+    pygame.draw.circle(screen, FILL, anchor, 16)
     pygame.draw.line(screen, FILL, bob, anchor, 2)
 
     pygame.display.update()
